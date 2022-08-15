b26696f434ca0bf3a4aa0a7587c75a462aaaf9f6
Alief <aliefiayasin@gmail.com>
2022-08-15 17:32:53 +0200

    submit

---
+++ client/src/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Form(){
+    return (
+        <>
+        <form>
+            <label>
+                Name:
+                <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+        </>
+    )
+}
+
+export default Hello;
